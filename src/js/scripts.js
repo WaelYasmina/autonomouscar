d8abf227e1d931ab72c0bd491bfa264f03c4dcb1
Wael Yasmina <Waelyasmina@outlook.com>
2022-06-05 09:37:06 +0100

    Project files

---
+++ src/js/scripts.js
@@ -0,0 +1,108 @@
+import * as THREE from 'three';
+import * as YUKA from 'yuka';
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
+
+const renderer = new THREE.WebGLRenderer({antialias: true});
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+renderer.setClearColor(0xA3A3A3);
+
+const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+camera.position.set(0, 10, 15);
+camera.lookAt(scene.position);
+
+const ambientLight = new THREE.AmbientLight(0x333333);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
+directionalLight.position.set(0, 10, 10);
+scene.add(directionalLight);
+
+const vehicle = new YUKA.Vehicle();
+
+function sync(entity, renderComponent) {
+    renderComponent.matrix.copy(entity.worldMatrix);
+}
+
+const path = new YUKA.Path();
+path.add( new YUKA.Vector3(-6, 0, 4));
+path.add( new YUKA.Vector3(-12, 0, 0));
+path.add( new YUKA.Vector3(-6, 0, -12));
+path.add( new YUKA.Vector3(0, 0, 0));
+path.add( new YUKA.Vector3(8, 0, -8));
+path.add( new YUKA.Vector3(10, 0, 0));
+path.add( new YUKA.Vector3(4, 0, 4));
+path.add( new YUKA.Vector3(0, 0, 6));
+
+path.loop = true;
+
+vehicle.position.copy(path.current());
+
+vehicle.maxSpeed = 3;
+
+const followPathBehavior = new YUKA.FollowPathBehavior(path, 3);
+vehicle.steering.add(followPathBehavior);
+
+const onPathBehavior = new YUKA.OnPathBehavior(path);
+//onPathBehavior.radius = 2;
+vehicle.steering.add(onPathBehavior);
+
+const entityManager = new YUKA.EntityManager();
+entityManager.add(vehicle);
+
+const loader = new GLTFLoader();
+loader.load('./assets/SUV.glb', function(glb) {
+    const model = glb.scene;
+    //model.scale.set(0.5, 0.5, 0.5);
+    scene.add(model);
+    model.matrixAutoUpdate = false;
+    vehicle.scale = new YUKA.Vector3(0.5, 0.5, 0.5);
+    vehicle.setRenderComponent(model, sync);
+});
+
+// const vehicleGeometry = new THREE.ConeBufferGeometry(0.1, 0.5, 8);
+// vehicleGeometry.rotateX(Math.PI * 0.5);
+// const vehicleMaterial = new THREE.MeshNormalMaterial();
+// const vehicleMesh = new THREE.Mesh(vehicleGeometry, vehicleMaterial);
+// vehicleMesh.matrixAutoUpdate = false;
+// scene.add(vehicleMesh);
+
+const position = [];
+for(let i = 0; i < path._waypoints.length; i++) {
+    const waypoint = path._waypoints[i];
+    position.push(waypoint.x, waypoint.y, waypoint.z);
+}
+
+const lineGeometry = new THREE.BufferGeometry();
+lineGeometry.setAttribute('position', new THREE.Float32BufferAttribute(position, 3));
+
+const lineMaterial = new THREE.LineBasicMaterial({color: 0xFFFFFF});
+const lines = new THREE.LineLoop(lineGeometry, lineMaterial);
+scene.add(lines);
+
+const time = new YUKA.Time();
+
+function animate() {
+    const delta = time.update().getDelta();
+    entityManager.update(delta);
+    renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
+window.addEventListener('resize', function() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
